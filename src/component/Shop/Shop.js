ea1037df19eb32decd3a833c77c968d287016dec
Siddika-akhter <siddika.tas28@gmail.com>
2020-08-23 16:31:31 +0600

    change commit

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../product/Product';
+
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10)
+    
+
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+            
+                {
+                    products.map(product=> <Product>{product.name}</Product>)
+                }
+            
+            </div>
+            <div className="card-container">
+                <h3>This is card </h3>
+            </div>
+            
+            
+        </div>
+    );
+};
+
+export default Shop;
