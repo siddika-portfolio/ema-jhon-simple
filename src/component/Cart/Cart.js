5bb037f2088f2bb2928cc8ac54a3506800605eeb
Siddika-akhter <siddika.tas28@gmail.com>
2020-09-02 14:24:23 +0600

    ema-jhon site ending thankyou

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -9,7 +9,7 @@ const Cart = (props) => {
     for(let i = 0; i< cart.length; i++){
         const product = cart[i];
         total = total + product.price * product.quantity;
-       
+        
     }
 
     let shipping = 0;
@@ -39,9 +39,7 @@ const Cart = (props) => {
             <p><small>Tax + VAT: {tax}</small></p>
             <p>Total Price:{grandTotal}</p>
             <br/>
-            <Link to="/review">
-                <button className="main-button">Review Order</button>
-            </Link>
+            { props.children}
         </div>
     );
 };
--- src/component/Review/Review.js
+++ src/component/Review/Review.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart, processOrder } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import Cart from '../Cart/Cart';
+import happyImage from '../../images/giphy.gif'
 
 const Review = () => {
     const [cart, setCart] = useState([]);
 
+    const [orderPlaced, setOrderPlaced] = useState(false);
+
+    const handlePlaceOrder = () => {
+        setCart([]);
+        setOrderPlaced(true);
+        processOrder();
+    }
     const removeProduct = (productKey) => {
         const newCart = cart.filter(pd => pd.key !== productKey);
         setCart(newCart);
@@ -28,6 +36,10 @@ const Review = () => {
         });
         setCart(cartProducts);
     }, [])
+    let thankyou; 
+    if(orderPlaced){
+        thankyou = <img src={happyImage} alt=""/>
+    }
     return (
         <div className="twin-container">
            <div className="product-container">
@@ -37,10 +49,13 @@ const Review = () => {
                         removeProduct = {removeProduct}
                         product={pd}>
                         </ReviewItem>)
-                }  
+                } 
+                {thankyou} 
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="main-button">Place Order</button>
+                </Cart>
             </div>
             
         </div>
